69438950be9f0e06d2338721de59c2f51bd47921
Sameer Rahmani <lxsameer@gnu.org>
2015-11-19 02:29:45 +0330

    Gniazdo added

--- alpha_client/project.clj
+++ alpha_client/project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [stylefruits/gniazdo "0.4.1"]]
   :main ^:skip-aot alpha-client.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- alpha_client/src/alpha_client/core.clj
+++ alpha_client/src/alpha_client/core.clj
@@ -1,5 +1,15 @@
 (ns alpha-client.core
+  [gniazdo.core :as ws]
   (:gen-class))
 
+
+(ws/send-msg socket "hello")
+(ws/close socket)
+
 (defn -main [& {:as args}]
+  (def socket
+    (ws/connect
+     (:connect args)
+     :on-receive #(prn 'received %)))
+
   (println args))
