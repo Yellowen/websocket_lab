f486bbfb853f227cac4328969703c57ddc1c064d
Sameer Rahmani <lxsameer@gnu.org>
2015-11-19 14:37:24 +0330

    basic command line client added

--- alpha_client/project.clj
+++ alpha_client/project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.2.374"]
                  [stylefruits/gniazdo "0.4.1"]]
   :main ^:skip-aot alpha-client.core
   :target-path "target/%s"
--- alpha_client/src/alpha_client/core.clj
+++ alpha_client/src/alpha_client/core.clj
@@ -1,15 +1,39 @@
 (ns alpha-client.core
-  [gniazdo.core :as ws]
-  (:gen-class))
+  (:require
+   [gniazdo.core :as ws]
+   [clojure.core.async :as async]))
 
 
-(ws/send-msg socket "hello")
-(ws/close socket)
+
+(defn command-help [socket ch & args]
+  (println "help"))
+
+(defn command-users [])
+
+(defn find-command [socket ch input]
+  (let [command (clojure.string/trim (first input))
+        func (ns-resolve 'alpha-client.core (symbol (str "command-" command)))]
+    (if func
+      (func socket ch (rest input))
+      (println (str "Can't find '" command "'.")))))
+
+(defn read-command []
+  (print "> ")
+  (flush)
+  (clojure.string/split (read-line) #" "))
 
 (defn -main [& {:as args}]
-  (def socket
-    (ws/connect
-     (:connect args)
-     :on-receive #(prn 'received %)))
+  (let [recv   (async/chan)
+        socket (ws/connect (get args "connect")
+                           :on-receive #(async/go (println (str "Log:" %))
+                                                  (async/>! recv %)))]
 
-  (println args))
+    (println (str "Connecting to " (get args "connect")))
+    (loop [input (read-command)]
+      (when-not (= "exit" (clojure.string/trim (first input)))
+        (try
+          (find-command socket recv input)
+          (catch Exception e (println e)))
+        (recur (read-command))))
+
+    (ws/close socket)))
